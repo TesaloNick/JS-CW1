1727545e0a2bfc1fcf3dc0eaaade97514960d893
Nikolay Antonovich <TesaloNick@gmail.com>
2021-01-29 20:47:59 +0300

    JS1

---
+++ script.js
@@ -0,0 +1,39 @@
+// document.write('Тест на дружелюбность');
+// let result = 0;
+// let emptyResult = 0;
+// const ask1 = prompt('Сколько у вас друзей?');
+// if (ask1 > 0){
+//     result++;
+// }else {
+//     emptyResult++;
+// }
+// const ask2 = prompt('Ты любишь общение?');
+// if (ask2.length) {
+//     result++;
+// } else {
+//     emptyResult++;
+// }
+// const ask3 = prompt('Сколько раз за сегодняшний день ты общался с людьми?');
+// if (ask3 > 0){
+//     result++;
+// }else {
+//     emptyResult++;
+// }
+// const ask4 = prompt('Ты девочка или мальчик?');
+// if (ask4.length) {
+//     result++;
+// } else {
+//     emptyResult++;
+// }
+
+// if (result==3) {
+//     alert('Ты дружелюбный')
+// } else {
+//     if (result==3){
+//         alert('Ты не совсем дружелюбный')
+//     } else{
+//         alert('Ты - псих-одиночка')
+//     }
+// }
+// console.log(result);
+// console.log(emptyResult);
