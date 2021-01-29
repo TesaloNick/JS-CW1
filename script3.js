4b15eb5c9694f9992dabc4cdc71f65598c58e6a8
Nikolay Antonovich <TesaloNick@gmail.com>
2021-01-29 21:35:16 +0300

    toUppercase1

---
+++ script3.js
@@ -0,0 +1,10 @@
+// let a = confirm('Тебе больше 5 лет?');
+// if (a) {
+//     alert('Тебе больше 5 лет')
+// } else {
+//     alert('тебе меньше 5 лет')
+// }
+
+const name1 = prompt('имя?');
+
+document.write('ваше имя начинается на ' + name1[0].toUpperCase());
