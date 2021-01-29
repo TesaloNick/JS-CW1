b2d0ef6f4ecb06f1d98cae8b1576e6665f6ca4e9
Nikolay Antonovich <TesaloNick@gmail.com>
2021-01-29 21:04:25 +0300

    JS2

--- script2.js
+++ script2.js
@@ -1,6 +1,29 @@
-const answer = prompt('какого цвета небо');
-if ((answer === 'голубое') || (answer === 'синее')) {
-    alert('правильно')
-} else {
-    alert('неправильно!!!')
+// const answer1 = prompt('какого цвета небо?');
+// const answer2 = prompt('дважды два?');
+
+// if (((answer1 === 'голубое') || (answer1 === 'синее')) && (answer2 === 4)) {
+//     alert('правильно')
+// } else {
+//     alert('неправильно!!!')
+// }
+const a = prompt('Первое число?');
+const с = prompt('Знак?');
+
+const b = prompt('Второе число?');
+
+switch (с) {
+    case '+':
+        alert(+a+(+b));
+        break;
+    case '-':
+        alert(+a-(+b));
+        break;
+    case '*':
+        alert(+a*(+b));
+        break;
+    case '/':
+        alert(+a/(+b));
+        break;
+    default:
+      alert( "ты мне втираешь какую-то дичь" );
 }
